b07e10580b46530659ad28c89604cf3db0c568c4
Jay Ganatra <jbg@somaiya.edu>
2021-03-25 13:34:18 +0530

    html files to php

--- allPoems.php
+++ allPoems.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,31 +8,34 @@
     <title>Random Poetry</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700&display=swap"
+        rel="stylesheet">
     <style>
-        body * {
-            font-family: 'Poppins', sans-serif;
-        }
+    body * {
+        font-family: 'Poppins', sans-serif;
+    }
     </style>
 </head>
+
 <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">Random Poetry</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <a class="navbar-brand" href="index.php">Random Poetry</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav">
                     <li class="nav-item">
-                        <a class="nav-link " aria-current="page" href="index.html">Home</a>
+                        <a class="nav-link " aria-current="page" href="index.php">Home</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link active" href="allPoems.html">All Poems</a>
+                        <a class="nav-link active" href="allPoems.php">All Poems</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="collection.php">Saved Collection</a>
-                    </li>  
+                    </li>
                 </ul>
             </div>
         </div>
@@ -45,14 +49,14 @@
     <div class="mx-3 my-5 rounded d-flex p-0 flex-wrap flex-column-reverse flex-md-row">
         <div class="col-md-3 pt-3 px-3">
             <h4>Authors</h4>
-            <div class="list-group" id="authorsList">                
+            <div class="list-group" id="authorsList">
             </div>
         </div>
         <div class="col-md-4 pt-3 px-3">
             <h4>Titles</h4>
             <div class="list-group" id="titlesList"></div>
         </div>
-        <div class="col-md-5 pt-3 px-3">            
+        <div class="col-md-5 pt-3 px-3">
             <div id="poemContainer"></div>
         </div>
     </div>
@@ -62,4 +66,5 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js"></script>
     <script src="./allPoems.js"></script>
 </body>
+
 </html>
--- collection.php
+++ collection.php
@@ -25,33 +25,35 @@ foreach ($data as $key => $poem) {
     <title>Random Poetry</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700&display=swap"
+        rel="stylesheet">
     <style>
-        body * {
-            font-family: 'Poppins', sans-serif;
-        }
+    body * {
+        font-family: 'Poppins', sans-serif;
+    }
 
-        li:not(.nav-item) {
-            list-style: none;
-            line-height: 2em;
-        }
+    li:not(.nav-item) {
+        list-style: none;
+        line-height: 2em;
+    }
     </style>
 </head>
 
 <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">Random Poetry</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <a class="navbar-brand" href="index.php">Random Poetry</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav">
                     <li class="nav-item">
-                        <a class="nav-link " aria-current="page" href="index.html">Home</a>
+                        <a class="nav-link" aria-current="page" href="index.php">Home</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="allPoems.html">All Poems</a>
+                        <a class="nav-link" href="allPoems.php">All Poems</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link active" href="collection.php">Saved Collection</a>
@@ -70,9 +72,11 @@ foreach ($data as $key => $poem) {
                     if ($key === 0) {
                         $class .= "  active";
                     } ?>
-                    <button class="<?php echo $class; ?>" id="<?php echo "v-pills-" . $key . "-tab"; ?>" data-bs-toggle="pill" data-bs-target="<?php echo "#v-pills-" . $key; ?>" type="button" role="tab" aria-controls="<?php echo "v-pills-" . $key; ?>" aria-selected="true">
-                        <?php echo $poem['title']; ?>
-                    </button>
+                <button class="<?php echo $class; ?>" id="<?php echo "v-pills-" . $key . "-tab"; ?>"
+                    data-bs-toggle="pill" data-bs-target="<?php echo "#v-pills-" . $key; ?>" type="button" role="tab"
+                    aria-controls="<?php echo "v-pills-" . $key; ?>" aria-selected="true">
+                    <?php echo $poem['title']; ?>
+                </button>
                 <?php } ?>
             </div>
             <div class="mx-2 col-md-7">
@@ -83,12 +87,13 @@ foreach ($data as $key => $poem) {
                         if ($index === 0) {
                             $class .= "  active";
                         } ?>
-                        <div class="<?php echo $class ?>" id="<?php echo "v-pills-" . $index; ?>" role="tabpanel" aria-labelledby="<?php echo "v-pills-" . $index . "-tab"; ?>">
-                            <div class="mx-4 mb-4 text-secondary fw-light">
-                                -Author: <?php echo $poem['author'] ?>
-                            </div>
-                            <?php echo str_replace("<li></li>", "<br />", base64_decode($poem['poem_lines'])) ?>
+                    <div class="<?php echo $class ?>" id="<?php echo "v-pills-" . $index; ?>" role="tabpanel"
+                        aria-labelledby="<?php echo "v-pills-" . $index . "-tab"; ?>">
+                        <div class="mx-4 mb-4 text-secondary fw-light">
+                            -Author: <?php echo $poem['author'] ?>
                         </div>
+                        <?php echo str_replace("<li></li>", "<br />", base64_decode($poem['poem_lines'])) ?>
+                    </div>
                     <?php
                     } ?>
                 </div>
